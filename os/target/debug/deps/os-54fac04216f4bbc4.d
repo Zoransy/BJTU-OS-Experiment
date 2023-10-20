b81a9523e370d03be1365ef8ac661c55443a966a
Zoransy <zoransy@outlook.com>
2023-10-20 11:33:14 +0800

    21301117 周书扬 Lab1

---
+++ os/target/debug/deps/os-54fac04216f4bbc4.d
@@ -0,0 +1,5 @@
+/mnt/os/target/debug/deps/os-54fac04216f4bbc4: src/main.rs
+
+/mnt/os/target/debug/deps/os-54fac04216f4bbc4.d: src/main.rs
+
+src/main.rs:
---
+++ os/target/debug/os.d
@@ -0,0 +1,1 @@
+/mnt/os/target/debug/os: /mnt/os/src/main.rs

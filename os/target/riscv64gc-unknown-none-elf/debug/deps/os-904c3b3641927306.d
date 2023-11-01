6d3e2ed57e848ad98711925b9a2a8eb496a9acc9
Zoransy <zoransy@outlook.com>
2023-11-01 17:13:26 +0800

    21301117 周书扬 lab1

---
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-904c3b3641927306.d
@@ -0,0 +1,5 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/debug/deps/os-904c3b3641927306: src/main.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/debug/deps/os-904c3b3641927306.d: src/main.rs
+
+src/main.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/debug/os.d
@@ -0,0 +1,1 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/debug/os: /mnt/os/src/main.rs
